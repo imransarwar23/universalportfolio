aa19bcb1e9327d2b735c531e5807f318f74bd1eb
imransarwar23 <imran.sarwar@students.mq.edu.au>
2025-07-07 22:49:01 +1000

    alternative methods for mfa not working either

--- webull_fetcher.py
+++ webull_fetcher.py
@@ -5,31 +5,37 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="confidential.env")
 
 def fetch_webull_data():
-    wb = webull()
-    email = os.getenv("WEBULL_EMAIL")
+    
+    phone = os.getenv("WEBULL_PHONE")
     password = os.getenv("WEBULL_PASSWORD")
 
     # Login
-    wb.get_mfa(email) #mobile number should be okay as well.
-    wb.get_security(email) #get your security question.
-    wb.login(email, password)
-    login_success = wb.login(email, password)
-    print("LOGIN SUCCESS:", login_success)
+    wb = webull()
+    wb.login(phone, password)
+    resp = wb.login(phone, password)
+    print("Login response:", resp)
+
+    if resp.get("success") is False:
+        print("❌ Login failed:", resp.get("msg", "Unknown error"))
+        exit(1)
+    else:
+        print("✅ Login succeeded")
+
 
     # Get open positions
-    positions = wb.get_positions()
+    # positions = wb.get_positions()
     
-    parsed = []
-    for p in positions:
-        if p['position']:
-            parsed.append({
-                "ticker": p["ticker"]["symbol"],
-                "position": float(p["position"]["position"]),
-                "averagePrice": float(p["position"]["costPrice"]),
-                "marketValue": float(p["position"]["marketValue"])
-            })
+    # parsed = []
+    # for p in positions:
+    #     if p['position']:
+    #         parsed.append({
+    #             "ticker": p["ticker"]["symbol"],
+    #             "position": float(p["position"]["position"]),
+    #             "averagePrice": float(p["position"]["costPrice"]),
+    #             "marketValue": float(p["position"]["marketValue"])
+    #         })
     
-    return parsed
+    # return parsed
 
 if __name__ == "__main__":
     portfolio = fetch_webull_data()
